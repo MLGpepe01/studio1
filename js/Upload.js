330eeb07f3f68c6e171d06e589866b4ab80aef87
MLGpepe01 <sliva@citypolyhs.org>
2018-11-19 17:33:00 -0500

    test 2

---
+++ js/Upload.js
@@ -0,0 +1,1 @@
+//this file will be used for uploading files onto the website
--- js/index.js
+++ js/index.js
@@ -4,3 +4,35 @@ import test from './script2.js';
 console.log('test');
 
 test();
+
+import * as upload from './Upload.js';
+import * as unpass from './usernameandpassword.js';
+import * as searchbar from './searchbar.js';
+import * as post from './post.js';
+
+//firebase.initializeApp(config);
+
+const database = firebase.database();
+const posts = database.ref('posts');
+
+// Initialize DOM refs
+const imagesDiv = document.getElementById('images');
+const imageForm = document.getElementById('image-form');
+const urlInput = document.getElementById('image-url');
+
+// Display images
+posts.on('child_added', data => {
+  let img = document.createElement('img');
+  img.src = data.val().image;
+  imagesDiv.appendChild(img);
+});
+
+// Add a new image
+imageForm.addEventListener('submit', e => {
+  e.preventDefault();
+
+  posts.push({ image: urlInput.value }, () => {
+    urlInput.value = ''; // signal complete
+  });
+
+});
