8da367eed951e2790ef47370bf77e972489a4f64
MLGpepe01 <sliva@citypolyhs.org>
2018-11-19 17:42:26 -0500

    []

--- js/Upload.js
+++ js/Upload.js
@@ -1,1 +1,27 @@
 //this file will be used for uploading files onto the website
+//firebase.initializeApp(config);
+
+const database = firebase.database();
+const posts = database.ref('posts');
+
+// Initialize DOM refs
+const imagesDiv = document.getElementById('images');
+const imageForm = document.getElementById('image-form');
+const urlInput = document.getElementById('image-url');
+
+// Display images
+posts.on('child_added', data => {
+  let img = document.createElement('img');
+  img.src = data.val().image;
+  imagesDiv.appendChild(img);
+});
+
+// Add a new image
+imageForm.addEventListener('submit', e => {
+  e.preventDefault();
+
+  posts.push({ image: urlInput.value }, () => {
+    urlInput.value = ''; // signal complete
+  });
+
+});
